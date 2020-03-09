syncany/syncany-plugin-sftp

commit 483a11a40122d4bd7eb03ff548c737a322109c52

SftpTransferManager: expose host key check choice
In particular for tests to be able to disable host key check, because
it will always fail with the embedded test SFTP server.

src/test/java/org/syncany/tests/plugin/sftp/SftpConnectionPluginTest.java
@@ -85,6 +85,7 @@ public class SftpConnectionPluginTest {
 		validSftpTransferSettings.setPassword("pass");
 		validSftpTransferSettings.setPort(EmbeddedSftpServerTest.PORT);
 		validSftpTransferSettings.setPath("/repo");		
+		validSftpTransferSettings.setCheckHostKey(false);
 	}
 	
 	@After
@@ -117,6 +118,7 @@ public class SftpConnectionPluginTest {
 		connection.setPassword("pass");
 		connection.setPort(EmbeddedSftpServerTest.PORT);
 		connection.setPath("/path/does/not/exist");
+		connection.setCheckHostKey(false);
 		
 		TransferManager transferManager = pluginInfo.createTransferManager(connection, null);
 		

src/main/java/org/syncany/plugins/sftp/SftpTransferManager.java
@@ -146,7 +146,7 @@ public class SftpTransferManager extends AbstractTransferManager {
 
 			// Initialize secure session, and connect
 			Properties properties = new Properties();
-			properties.put("StrictHostKeyChecking", "ask");
+			properties.put("StrictHostKeyChecking", getSettings().getCheckHostKeyAsString());
 
 			secureSession = secureChannel.getSession(getSettings().getUsername(), getSettings().getHostname(), getSettings().getPort());
 			secureSession.setConfig(properties);

src/main/java/org/syncany/plugins/sftp/SftpTransferSettings.java
@@ -62,6 +62,10 @@ public class SftpTransferSettings extends TransferSettings {
 	@Setup(order = 6, description = "Port")
 	private int port = 22;
 
+	@Element(name = "checkHostKey", required = false)
+	@Setup(order = 7, description = "Whether to check the server key against known hosts (if not set, then prompt the user whether to add the key if it does not match)")
+	private Boolean checkHostKey = null;
+
 	public String getHostname() {
 		return hostname;
 	}
@@ -108,6 +112,18 @@ public class SftpTransferSettings extends TransferSettings {
 
 	public void setPrivateKey(File privateKey) {
 		this.privateKey = privateKey;
+	}
+
+	public Boolean getCheckHostKey() {
+		return checkHostKey;
+	}
+
+	public String getCheckHostKeyAsString() {
+		return checkHostKey == null ? "ask" : (checkHostKey ? "yes" : "no");
+	}
+
+	public void setCheckHostKey(Boolean checkHostKey) {
+		this.checkHostKey = checkHostKey;
 	}
 
 	@Validate

commit e23c669a84e3ec6934d5b12ed336332101dcc619

test: add a check for TransferSettings fields
Catches bug introduced in 483a11a40122d4bd7eb03ff548c737a322109c52
(SftpTransferManager: expose host key check choice): nullable Boolean is
not supported as a field type by core/ (fails at runtime with
java.lang.RuntimeException: Unknown type: class java.lang.Boolean).

src/test/java/org/syncany/tests/plugin/sftp/SftpConnectionPluginTest.java
@@ -43,6 +43,9 @@ import org.syncany.plugins.sftp.SftpTransferSettings;
 import org.syncany.plugins.transfer.StorageException;
 import org.syncany.plugins.transfer.TransferManager;
 import org.syncany.plugins.transfer.TransferPlugin;
+import org.syncany.plugins.transfer.TransferPluginOption;
+import org.syncany.plugins.transfer.TransferPluginOption.ValidationResult;
+import org.syncany.plugins.transfer.TransferPluginOptions;
 import org.syncany.plugins.transfer.files.RemoteFile;
 import org.syncany.plugins.transfer.files.MultichunkRemoteFile;
 import org.syncany.tests.util.TestFileUtil;
@@ -132,6 +135,37 @@ public class SftpConnectionPluginTest {
 		// This should cause a Storage exception, because the path does not exist
 		transferManager.connect();	
 		transferManager.init(true);
+	}
+
+	@Test
+	public void testTransferSettingsFields() throws StorageException {
+		// Checks the fields types and annotations to catch runtime errors
+
+		SftpTransferSettings settings = validSftpTransferSettings;
+
+		List<TransferPluginOption> pluginOptions =
+				TransferPluginOptions.getOrderedOptions(settings.getClass());
+
+		for (TransferPluginOption option : pluginOptions) {
+			String fieldName = option.getName();
+			String fieldValue = settings.getField(fieldName);
+			ValidationResult validationRes = option.isValid(fieldValue);
+			assertEquals("Value '" + fieldValue +  "' of field '" + fieldName +
+					"' must pass validity check",
+					ValidationResult.VALID, validationRes);
+		}
+	}
+
+	@Test(expected=StorageException.class)
+	public void testInvalidSettingsValidation() throws StorageException {
+		assertFalse(invalidSftpTransferSettings.isValid());
+		invalidSftpTransferSettings.validateRequiredFields();
+	}
+
+	@Test
+	public void testValidSettingsValidation() throws StorageException {
+		assertTrue(validSftpTransferSettings.isValid());
+		validSftpTransferSettings.validateRequiredFields();
 	}
 	
 	@Test(expected=StorageException.class)